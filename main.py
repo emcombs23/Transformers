a20575b3f0b71eac21c837f5caa85c35c61d6219
emcombs23 <emcombs.23.sv@gmail.com>
2026-04-20 13:50:13 +0000

    embedding

--- main.py
+++ main.py
@@ -14,6 +14,23 @@ for char in vocab:
     count +=1
 print(vocabDict)
 
+def clean_string(string):
+    string = string.lower()
+    cleanString = ""
+    for char in string:
+        if char in vocabDict:
+            cleanString += char
+        else:
+            cleanString += " "
+    newText = re.sub(r'\s+', ' ', cleanString)
+    return newText
+
+print(clean_string("Hello, World!x"))
+
+embedding = nn.Embedding(27,2)
+
+
+
 @app.get("/vocab")
 def get_chars():
     return vocabDict
@@ -21,12 +38,18 @@ def get_chars():
 @app.get("/encode")
 def encode_string(string: str):
     string = string.lower()
+    clean = clean_string(string)
     encodeList = []
-    for char in string:
-        if char in vocabDict:
-            encodeList.append(vocabDict[char])
-        else:
-            encodeList.append(vocabDict[" "])
+    for char in clean:
+        encodeList.append(vocabDict[char])
     return encodeList
+
+@app.get("/embed")
+def embed_string():
+    result = {}
+    for i in range(27):
+        vector = embedding.weight[i]
+        result[i] = vector.tolist()
+    return result
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
